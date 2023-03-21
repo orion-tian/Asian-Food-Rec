2358fff751019cb8ba43973796a97a9aa2035815
Rachel Zhou <sz287@cornell.edu>
2023-03-20 20:37:08 -0400

    added chinese, didn't really fix it

--- filter_asian.py
+++ filter_asian.py
@@ -5,7 +5,7 @@ import pickle
 df = pd.read_csv('RAW_recipes.csv')
 
 # filter the rows based on the "tags" column
-df = df[df['tags'].str.contains('asian', case=False)]
+df = df[df['tags'].str.contains('asian|chinese|japanese|korean|indian', case=False)]
 
 # save the filtered dataset as a pickle file
 with open('filtered_dataset.pickle', 'wb') as f:
@@ -13,3 +13,4 @@ with open('filtered_dataset.pickle', 'wb') as f:
 
 
 df.to_csv('filtered_dataset.csv', index=False)
+print(len(df))
