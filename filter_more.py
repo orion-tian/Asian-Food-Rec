a9c594d8448e43a04fe82a3212ad474d480856d0
Orion <owt4@cornell.edu>
2023-03-27 11:06:04 -0400

    upload data as sql correctly
    - replaced all '%' with ' percent' in filtered_dataset using replace all

--- filter_more.py
+++ filter_more.py
@@ -1,3 +1,4 @@
+# file no longer needed
 import pandas as pd
 import pickle
 
@@ -6,20 +7,18 @@ df = pd.read_csv('filtered_dataset.csv')
 
 # # get rid of unnecessary columns for now
 # # name,id,minutes,contributor_id,submitted,tags,nutrition,n_steps,steps,description,ingredients,n_ingredients
+df = df[:500]
 # df = df.drop('id', axis=1)
 # df = df.drop('minutes', axis=1)
 # df = df.drop('contributor_id', axis=1)
 # df = df.drop('submitted', axis=1)
-# df = df.drop('tags', axis=1)
+# # df = df.drop('tags', axis=1)
 # df = df.drop('nutrition', axis=1)
 # df = df.drop('n_steps', axis=1)
 # df = df.drop('steps', axis=1)
 # df = df.drop('description', axis=1)
 # df = df.drop('n_ingredients', axis=1)
-
-for i in range(1500, 13908):
-  df = df.drop(i)
-
+# df = df[:500]
 
 # save the filtered dataset as a pickle file
 with open('filtered_more.pickle', 'wb') as f:
