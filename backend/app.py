4a265dd9d2e74fd0f04f3e46c6a8c4151fee6210
Lee, Derek <Dereklee9ab@aol.com>
2023-03-27 22:24:27 -0400

    First Prototype

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
 # Don't worry about the deployment credentials, those are fixed
 # You can use a different DB name if you want to
 MYSQL_USER = "root"
-MYSQL_USER_PASSWORD = ""
+MYSQL_USER_PASSWORD = "mmmm"
 MYSQL_PORT = 3306
 MYSQL_DATABASE = "recipes"
 
@@ -27,110 +27,138 @@ mysql_engine.load_file_into_db()
 app = Flask(__name__)
 CORS(app)
 
+_keys = ['name', 'id', 'minutes', 'tags', 'nutrition', 'steps', 'description', 'ingredients']
+
 # Sample search, the LIKE operator in this case is hard-coded, 
 # but if you decide to use SQLAlchemy ORM framework, 
 # there's a much better and cleaner way to do this
+
 def sql_search(episode):
     query_sql = f"""SELECT * FROM episodes WHERE LOWER( title ) LIKE '%%{episode.lower()}%%' limit 10"""
     keys = ["id","title","descr"]
     data = mysql_engine.query_selector(query_sql)
     return json.dumps([dict(zip(keys,i)) for i in data])
 
-def boolean_search(ingred_lst, query_lst):
+def boolean_search(ingred_lst):
     ingred_postings = []
-    for ingred in ingred_lst:
+
+    if len(ingred_lst) == 0:
         mysql_engine.query_selector(f"""USE recipes""")
-        query_sql = f"""SELECT posting FROM inverted_index WHERE ingredient = '{ingred.lower()}' """ 
+        query_sql = f"""SELECT id FROM mytable WHERE id > 0 AND id <= 10000 LIMIT 1000""" 
         data = mysql_engine.query_selector(query_sql)
         
         posting_set = set()
         data = [str(i) for i in data]
         for tup in data:
-            tup = tup[3:len(tup)-4]
+            tup = tup[1:-2]
             for p in tup.split(','):
                 posting_set.add(p.strip())
-
         ingred_postings.append(posting_set)
+    else:
+        for ingred in ingred_lst:
+            mysql_engine.query_selector(f"""USE recipes""")
+            query_sql = f"""SELECT posting FROM inverted_index WHERE ingredient = '{ingred.lower()}' """ 
+            data = mysql_engine.query_selector(query_sql)
+            
+            posting_set = set()
+            data = [str(i) for i in data]
+            for tup in data:
+                tup = tup[3:len(tup)-4]
+                for p in tup.split(','):
+                    posting_set.add(p.strip())
 
-        # print(ingred_postings)
+            ingred_postings.append(posting_set)
 
+    # print(ingred_postings)
     #perform boolean and on all postings
     result_postings = ingred_postings[0]
+    # print(result_postings)
     for i in range(1, len(ingred_postings)):
         curr = ingred_postings[i]
         result_postings = curr.intersection(result_postings)
     # print(result_postings)
 
     result = []
-    keys = ['name', 'id', 'minutes', 'tags', 'nutrition', 'steps', 'description', 'ingredients']
-    
+
     for p in result_postings:
         mysql_engine.query_selector(f"""USE recipes""")
         query_sql = f"""SELECT name, id, minutes, tags, nutrition, steps, description, ingredients FROM mytable WHERE id = '{p}' """ 
         data = mysql_engine.query_selector(query_sql)
-        for i in data:
-            result.append(dict(zip(keys,i)))
+        for j, val in enumerate(data):
+            if j == 4:
+                print(val)
+                print(type(val))
+            values = []
+            for i in val:
+                if type(i) == str and len(i) > 0 and i[0] == '[':
+                    curVal = i.strip('][').replace("'","").split(', ')
+                    try:
+                        values.append(int(curVal))
+                    except Exception:
+                        values.append(curVal)
+                else:
+                    values.append(i)
+            
+            result.append(dict(zip(_keys, values)))
+
 
     return result
 
 def subset_search(ingred_lst):
     mysql_engine.query_selector(f"""USE recipes""")
-    query_sql = f"""SELECT ingredient from inverted_index WHERE ingredient NOT IN {repr(tuple(map(str, ingred_lst)))}"""
+    if len(ingred_lst) == 1:
+        query_sql = f"""SELECT ingredient from inverted_index WHERE ingredient NOT IN ('{ingred_lst[0]}')"""
+    else:
+        query_sql = f"""SELECT ingredient from inverted_index WHERE ingredient NOT IN {repr(tuple(map(str, ingred_lst)))}"""
+
+    print(query_sql)
     data = mysql_engine.query_selector(query_sql)
-    data = [str(i) for i in data]
+    data = [str(i) for i in data][1:-1]
     not_ingred_lst = []
 
     for tup in data:
-        tup = tup[3:len(tup)-4]
+        tup = tup[2:len(tup)-3]
         for p in tup.split(','):
-            not_ingred_lst.add(p.strip())
+            not_ingred_lst.append(p.strip())
+    all_posting_set = set()
 
     print(not_ingred_lst)
-    all_posting_set = set()
 
     mysql_engine.query_selector(f"""USE recipes""")
     query_sql = f"""SELECT posting FROM inverted_index""" 
     data = mysql_engine.query_selector(query_sql)
-    data = [str(i) for i in data]
+    data = [str(i) for i in data][1:-1]
+    
     for tup in data:
-        tup = tup[3:len(tup)-4]
+        tup = tup[3:-4]
         for p in tup.split(','):
             all_posting_set.add(p.strip())
     
-    not_ingred_postings = []
+    not_ingred_postings = set()
 
     for ingred in not_ingred_lst:
         mysql_engine.query_selector(f"""USE recipes""")
-        query_sql = f"""SELECT posting FROM inverted_index WHERE ingredient = '{ingred.lower()}' """ 
+        ingred_new = ingred.lower().replace("'","\\\'")
+        query_sql = f"""SELECT posting FROM inverted_index WHERE ingredient = '{ingred_new}' """ 
         data = mysql_engine.query_selector(query_sql)
         
-        posting_set = set()
         data = [str(i) for i in data]
         for tup in data:
-            tup = tup[3:len(tup)-4]
+            tup = tup[3:-3]
             for p in tup.split(','):
-                posting_set.add(p.strip())
+                not_ingred_postings.add(p.strip())
 
-        not_ingred_postings.append(posting_set)
+    diff = all_posting_set.difference(not_ingred_postings)
 
-    #perform boolean or on all postings
-    result_postings = not_ingred_postings[0]
-    for i in range(1, len(not_ingred_postings)):
-        curr = not_ingred_postings[i]
-        result_postings = curr.union(result_postings)
-
-    diff = all_posting_set.difference(result_postings)
-
-    
     result = []
-    keys = ['name', 'id', 'minutes', 'tags', 'nutrition', 'steps', 'description', 'ingredients']
+    
     
     for p in diff:
         mysql_engine.query_selector(f"""USE recipes""")
         query_sql = f"""SELECT name, id, minutes, tags, nutrition, steps, description, ingredients FROM mytable WHERE id = '{p}' """ 
         data = mysql_engine.query_selector(query_sql)
         for i in data:
-            result.append(dict(zip(keys,i)))
+            result.append(dict(zip(_keys,i)))
 
     return result
 
@@ -164,16 +192,25 @@ def recipes_search():
     body = request.json
     query = body['query']  # string - this is the freeform query
     pantry = body['pantry']  # string list - this is the list of ingredients
-
+    print(query)
+    print(pantry)
     # Output is a list of dicts on information about each recipe (jsonified)
-    subsets = subset_search(pantry)
+    subsets = boolean_search(pantry)
+    print(len(subsets))
     
-    descriptions = [recipe['description'] for recipe in subsets]
+    descriptions = [str(recipe['name']) + str(recipe['description']) + str(recipe['steps']) for recipe in subsets]
 
     indices = jaccard_similarity(descriptions, query)
 
-    ranked = [subsets[i] for i in indices]
-
+    ranked = [subsets[i] for i in indices][1:9]
+    print(len(ranked))
     return json.dumps(ranked)
 
-# app.run(debug=True)
+@app.route("/recipes/<int:id>")
+def recipes(id):
+    mysql_engine.query_selector(f"""USE recipes""")
+    query_sql = f"""SELECT name, id, minutes, tags, nutrition, steps, description, ingredients FROM mytable WHERE id = '{id}' """ 
+    data = mysql_engine.query_selector(query_sql)
+    return dict(zip(_keys,data[0]))
+
+app.run(debug=True)
