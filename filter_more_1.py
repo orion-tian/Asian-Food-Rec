81b1da57f5d9c3c3423d0b5bc956cc259c5bb67d
Lee, Derek <Dereklee9ab@aol.com>
2023-04-12 15:37:12 -0400

    Made the csv not have quotes around ingredients

--- filter_more_1.py
+++ filter_more_1.py
@@ -4,5 +4,7 @@ df = pd.read_csv('filtered_dataset.csv')
 df['ingredients'] = df['ingredients'].str.strip('[]').str.split(', ')
 df = df.explode('ingredients')
 
+df['ingredients'] = df['ingredients'].str.strip('\'')
+
 ingredient_recipe_df = df.rename(columns={'ingredients': 'Ingredient', 'id': 'id'})[['Ingredient', 'id']]
 ingredient_recipe_df.to_csv('ingredients.csv', index=False)
