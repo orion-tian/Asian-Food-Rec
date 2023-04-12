66b66e146ae0341dcd1330bdaaf1ada266ac66a9
Lee, Derek <Dereklee9ab@aol.com>
2023-04-12 15:55:59 -0400

    Sorted same ingredient next to each other

--- filter_more_1.py
+++ filter_more_1.py
@@ -3,8 +3,8 @@ import pandas as pd
 df = pd.read_csv('filtered_dataset.csv')
 df['ingredients'] = df['ingredients'].str.strip('[]').str.split(', ')
 df = df.explode('ingredients')
-
-df['ingredients'] = df['ingredients'].str.strip('\'')
+df['ingredients'] = df['ingredients'].str.strip('\'\"')
+df.sort_values(by=['ingredients', 'id'], inplace=True)
 
 ingredient_recipe_df = df.rename(columns={'ingredients': 'Ingredient', 'id': 'id'})[['Ingredient', 'id']]
 ingredient_recipe_df.to_csv('ingredients.csv', index=False)
