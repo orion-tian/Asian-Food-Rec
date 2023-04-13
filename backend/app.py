202b7d1170e29aadcc7884bb0496d5c04c919086
Lee, Derek <Dereklee9ab@aol.com>
2023-04-12 20:38:52 -0400

    Subset search working, boolean search working, way faster

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ from jaccard import jaccard_similarity
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 # import boolean_search
 import pickle
+import ast
 
 # ROOT_PATH for linking with all your files. 
 # Feel free to use a config.py or settings.py with a global export variable
@@ -30,87 +31,105 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 _keys = ['name', 'id', 'minutes', 'tags', 'nutrition', 'steps', 'description', 'ingredients']
 
-# Sample search, the LIKE operator in this case is hard-coded, 
-# but if you decide to use SQLAlchemy ORM framework, 
-# there's a much better and cleaner way to do this
+def get_postings_from_data(data):
+    data = [str(i) for i in data]
 
-def sql_search(episode):
-    query_sql = f"""SELECT * FROM episodes WHERE LOWER( title ) LIKE '%%{episode.lower()}%%' limit 10"""
-    keys = ["id","title","descr"]
+    postings = set()
+
+    for tup in data:
+        tup = tup[1:-2]
+        postings.add(int(tup))
+
+    return postings
+
+def get_sql_tuple_from_postings(postings):
+    return "(" + str(postings)[1:-1] + ")"
+
+def get_sql_tuple_from_ingredients(ingredient_lst):
+    return "(" + str(ingredient_lst)[1:-1].replace("%", "%%") + ")"
+
+def get_recipes_from_postings(result_postings):
+    if len(result_postings) == 0:
+        return []
+    postings_str = get_sql_tuple_from_postings(result_postings)
+    mysql_engine.query_selector(f"""USE recipes""")
+    query_sql = f"""WITH ings AS (
+                        SELECT inverted_index.id, CONCAT("[", group_concat(inverted_index.ingredient), "]") AS ingredients 
+                        FROM inverted_index GROUP BY inverted_index.id
+                    )
+                    SELECT name, mytable.id, minutes, tags, nutrition, steps, description, ings.ingredients 
+                    FROM mytable 
+                    INNER JOIN ings
+                    ON mytable.id = ings.id
+                    WHERE mytable.id IN {postings_str}""" 
+    
     data = mysql_engine.query_selector(query_sql)
-    return json.dumps([dict(zip(keys,i)) for i in data])
+    data = [str(i) for i in data]
+    result = []
+    for recipe in data:
+        tup = ast.literal_eval(recipe)
+        values = []
+        for val in tup:
+            if type(val) == str and len(val) > 0 and val[0] == '[':
+                curVal = val.strip('][').replace("'","").split(',')
+                curVal = [v.strip() for v in curVal]
+                try:
+                    values.append(int(curVal))
+                except Exception:
+                    values.append(curVal)
+            else:
+                values.append(val)
+            
+        result.append(dict(zip(_keys, values)))
+    
+    return result
+
+# returns postings that contain any of the ingredients in ingredients list
+def get_postings_containing_ingredients(ingredient_lst):
+    ingred_str = get_sql_tuple_from_ingredients(ingredient_lst)
+
+    mysql_engine.query_selector(f"""USE recipes""")
+
+    query_sql = f"""SELECT DISTINCT id FROM inverted_index WHERE ingredient in {ingred_str}""" 
+    data = mysql_engine.query_selector(query_sql)
+    
+    return get_postings_from_data(data)
+
+def get_postings_containing_all_ingredients(ingredient_lst):
+    ingred_str = get_sql_tuple_from_ingredients(ingredient_lst)
+    length = len(ingredient_lst)
+
+    mysql_engine.query_selector(f"""USE recipes""")
+    query_sql = f"""SELECT DISTINCT id FROM inverted_index WHERE ingredient in {ingred_str} GROUP BY id HAVING COUNT(*) = {length}""" 
+
+    data = mysql_engine.query_selector(query_sql)
+
+    return get_postings_from_data(data)
 
 def boolean_search(ingred_lst):
-    ingred_postings = []
-
+    posting_set = set()
     if len(ingred_lst) == 0:
         mysql_engine.query_selector(f"""USE recipes""")
-        query_sql = f"""SELECT id FROM mytable WHERE id > 0 AND id <= 10000 LIMIT 1000""" 
+        query_sql = f"""SELECT id FROM mytable LIMIT 1000""" 
         data = mysql_engine.query_selector(query_sql)
-        
-        posting_set = set()
-        data = [str(i) for i in data]
-        for tup in data:
-            tup = tup[1:-2]
-            for p in tup.split(','):
-                posting_set.add(p.strip())
-        ingred_postings.append(posting_set)
+        posting_set = get_postings_from_data(data)
     else:
-        for ingred in ingred_lst:
-            mysql_engine.query_selector(f"""USE recipes""")
-            query_sql = f"""SELECT posting FROM inverted_index WHERE ingredient = '{ingred.lower()}' """ 
-            data = mysql_engine.query_selector(query_sql)
-            
-            posting_set = set()
-            data = [str(i) for i in data]
-            for tup in data:
-                tup = tup[3:len(tup)-4]
-                for p in tup.split(','):
-                    posting_set.add(p.strip())
+        posting_set = get_postings_containing_all_ingredients(ingred_lst)
 
-            ingred_postings.append(posting_set)
-
-    # print(ingred_postings)
-    #perform boolean and on all postings
-    result_postings = ingred_postings[0]
-    # print(result_postings)
-    for i in range(1, len(ingred_postings)):
-        curr = ingred_postings[i]
-        result_postings = curr.intersection(result_postings)
-    # print(result_postings)
-
-    result = []
-
-    for p in result_postings:
-        mysql_engine.query_selector(f"""USE recipes""")
-        query_sql = f"""SELECT name, id, minutes, tags, nutrition, steps, description, ingredients FROM mytable WHERE id = '{p}' """ 
-        data = mysql_engine.query_selector(query_sql)
-        for j, val in enumerate(data):
-            if j == 4:
-                print(val)
-                print(type(val))
-            values = []
-            for i in val:
-                if type(i) == str and len(i) > 0 and i[0] == '[':
-                    curVal = i.strip('][').replace("'","").split(', ')
-                    try:
-                        values.append(int(curVal))
-                    except Exception:
-                        values.append(curVal)
-                else:
-                    values.append(i)
-            
-            result.append(dict(zip(_keys, values)))
-
-
-    return result
+    if len(posting_set) > 0:
+        return get_recipes_from_postings(posting_set)
+    else:
+        return boolean_search([])
 
 def subset_search(ingred_lst):
     mysql_engine.query_selector(f"""USE recipes""")
+    print(ingred_lst)
+    if len(ingred_lst) == 0:
+        return boolean_search(ingred_lst)
     if len(ingred_lst) == 1:
-        query_sql = f"""SELECT ingredient from inverted_index WHERE ingredient NOT IN ('{ingred_lst[0]}')"""
+        query_sql = f"""SELECT DISTINCT ingredient from inverted_index WHERE ingredient NOT IN ('{ingred_lst[0]}')"""
     else:
-        query_sql = f"""SELECT ingredient from inverted_index WHERE ingredient NOT IN {repr(tuple(map(str, ingred_lst)))}"""
+        query_sql = f"""SELECT DISTINCT ingredient from inverted_index WHERE ingredient NOT IN {repr(tuple(map(str, ingred_lst)))}"""
 
     print(query_sql)
     data = mysql_engine.query_selector(query_sql)
@@ -118,51 +137,26 @@ def subset_search(ingred_lst):
     not_ingred_lst = []
 
     for tup in data:
-        print(tup)
         tup = tup[2:len(tup)-3]
         for p in tup.split(','):
             not_ingred_lst.append(p.strip())
     all_posting_set = set()
 
-    print(not_ingred_lst)
-
     mysql_engine.query_selector(f"""USE recipes""")
-    query_sql = f"""SELECT posting FROM inverted_index""" 
+    query_sql = f"""SELECT id FROM inverted_index""" 
     data = mysql_engine.query_selector(query_sql)
-    data = [str(i) for i in data][1:-1]
     
-    for tup in data:
-        tup = tup[3:-4]
-        for p in tup.split(','):
-            all_posting_set.add(p.strip())
-    
-    not_ingred_postings = set()
+    all_posting_set = get_postings_from_data(data)
+    all_posting_set.remove(0)
 
-    for ingred in not_ingred_lst:
-        mysql_engine.query_selector(f"""USE recipes""")
-        ingred_new = ingred.lower().replace("'","\\\'")
-        query_sql = f"""SELECT posting FROM inverted_index WHERE ingredient = '{ingred_new}' """ 
-        data = mysql_engine.query_selector(query_sql)
-        
-        data = [str(i) for i in data]
-        for tup in data:
-            tup = tup[3:-3]
-            for p in tup.split(','):
-                not_ingred_postings.add(p.strip())
+    not_ingred_postings = get_postings_containing_ingredients(not_ingred_lst)
 
     diff = all_posting_set.difference(not_ingred_postings)
-
-    result = []
     
-    
-    for p in diff:
-        mysql_engine.query_selector(f"""USE recipes""")
-        query_sql = f"""SELECT name, id, minutes, tags, nutrition, steps, description, ingredients FROM mytable WHERE id = '{p}' """ 
-        data = mysql_engine.query_selector(query_sql)
-        for i in data:
-            result.append(dict(zip(_keys,i)))
-
-    return result
+    if len(diff) > 0:
+        return get_recipes_from_postings(diff)
+    else:
+        return boolean_search(ingred_lst)
 
 def sql_recipe_search(ingred_lst_str):
     ingred_lst = []
@@ -200,7 +194,7 @@ def recipes_search():
     # Output is a list of dicts on information about each recipe (jsonified)
     
     # subsets = boolean_search(pantry)
-    
+
     subsets = subset_search(pantry)
     
     print(len(subsets))
@@ -209,7 +203,7 @@ def recipes_search():
 
     indices = jaccard_similarity(descriptions, query)
 
-    ranked = [subsets[i] for i in indices][1:9]
+    ranked = [subsets[i] for i in indices][0:9]
     print(len(ranked))
     return json.dumps(ranked)
 
